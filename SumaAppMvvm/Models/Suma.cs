668d73f729142337baecda554199a0e2acf06bde
MoiPad92 <moipad85@gmail.com>
2024-03-10 12:16:09 -0600

    Agregar archivos de proyecto.

---
+++ SumaAppMvvm/Models/Suma.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace SumaAppMvvm.Models
+{
+    internal class Suma
+    {
+        public double Valor1 { get; set; }
+        public double Valor2 { get; set;}
+
+
+        public double SumarValores()
+        {
+            return Valor1 + Valor2;
+        }
+    }
+}
---
+++ SumaAppMvvm/ViewModels/MainPageViewModels.cs
@@ -0,0 +1,77 @@
+﻿
+using SumaAppMvvm.Models;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SumaAppMvvm.ViewModels
+{
+    internal partial class MainPageViewModels : ObservableObject
+    {
+        [ObservableProperty]
+        private double _valor1;
+
+        [ObservableProperty] 
+        private double _valor2;
+
+        [ObservableProperty]
+        private double _Resultado;
+
+        [RelayCommand]
+        private void SumarValores()
+        {
+            try
+            {
+                if (Validar())
+                { 
+                    Suma suma = new Suma();
+                    suma.Valor1 = _valor1;
+                    suma.Valor2 = _valor2;
+                    Resultado = suma.SumarValores();
+                }
+            }
+            catch (Exception ex) 
+            {
+                Alerta("Error: ", $"Se obtuvo una excepción: {ex.Message}");
+            }
+        
+        }
+
+        [RelayCommand]
+        private void Limpiar() 
+        {
+            Valor1 = 0;
+            Valor2 = 0;
+            Resultado = 0;
+        }
+
+        private bool Validar()
+        {
+            if (Valor1 == 0)
+            {
+                Alerta("Avertencia!","Los campos no pueden ir vacios, ingrese el primer valor");
+                return false;
+            }
+            else if (Valor2 == 0)
+            {
+                Alerta("Avertencia!", "Los campos no pueden ir vacios, ingrese el segundo valor");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Método personalizable para construir alertas
+        /// </summary>
+        /// <param name="Tipo">Tipo de Alerta</param>
+        /// <param name="Mensaje">Mensaje de alerta</param>
+        public void Alerta(String Tipo, String Mensaje)
+        {
+            MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert(Tipo, Mensaje, "Aceptar"));
+        }
+
+    }
+
+}
